59a35dfe39755231d68ced5ca4b42de4db52b502
subbu <padalasubbu80@gmail.com>
2020-03-30 18:48:10 +0530

    Cucumber Options updated.

--- CarRentalBDD3/src/test/java/allInOnePackage/Ex1TestRunner.java
+++ CarRentalBDD3/src/test/java/allInOnePackage/Ex1TestRunner.java
@@ -5,7 +5,12 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions()
+@CucumberOptions(
+		//the path of the feature files
+				features = "src\\test\\java\\allInOnePackage\\ex1login.feature",
+				//the path of the step definition files
+				glue={"allInOnePackage"}
+		)
 
 public class Ex1TestRunner {
 
